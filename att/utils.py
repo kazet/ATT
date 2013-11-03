874e4ac0b3e493695b2722b4de259f08789e8301
Krzysztof Zajac <kazet@0x04.net>
2013-11-03 01:10:23 +0100

    att.utils formatting and docstrings

--- att/utils.py
+++ att/utils.py
@@ -1,12 +1,18 @@
 import math
 
 def First(tup):
+  """Return the first element of a tuple or a list. Just for verboseness
+  of code that uses it."""
   return tup[0]
 
 def Second(tup):
+  """Return the second element of a tuple or a list. Just for verboseness
+  of code that uses it."""
   return tup[1]
 
 def GroupByKey(data):
+  """Groups the list by its keys. This function will return a list of buckets,
+  each of them containing all list elements with the same key."""
   data.sort(key=lambda (key, unused_value): key)
   buckets = []
   last = None
@@ -23,10 +29,12 @@ def GroupByKey(data):
 
 
 def Average(lst):
+  """Return the list average."""
   return sum(lst) / float(len(lst))
 
 
 def Gauss(x, standard_deviation=1, center=0):
+  """Calculate the normal distribution."""
   return \
     math.exp((-(x - center) ** 2) / 2 * standard_deviation * 2) / \
     (standard_deviation * math.sqrt(2.0 * math.pi))
@@ -34,47 +42,54 @@ def Gauss(x, standard_deviation=1, center=0):
 
 def SetSimilarity(s1, s2):
   """Returns the size of the largest common subset of s1 and s2
-  divided by their length combined."""
+  divided by their length combined or 0 if both are empty."""
   if not s1 and not s2:
     return 0
   return len(s1 & s2) / float(len(s1) + len(s2))
 
 def NumCommonElements(s1, s2):
+  """Return the number of common elements in s1 and s2."""
   if not s1 and not s2:
     return 0
-  s1_s = sorted(s1)
-  s2_s = sorted(s2)
-  i = 0
-  j = 0
-  common = 0
-  while i < len(s1_s) and j < len(s2_s):
-    if s1_s[i] < s2_s[j]:
-      i += 1
-    elif s1_s[i] > s2_s[j]:
-      j += 1
+  s1_sorted = sorted(s1)
+  s2_sorted = sorted(s2)
+  s1_iter = 0
+  s2_iter = 0
+  num_common = 0
+  while s1_iter < len(s1_sorted) and j < len(s2_sorted):
+    if s1_sorted[s1_iter] < s2_sorted[s2_iter]:
+      s1_iter += 1
+    elif s1_sorted[s1_iter] > s2_sorted[s2_iter]:
+      s2_iter += 1
     else:  # equal
-      common += 1
-      i += 1
-      j += 1
-  return common
+      num_common += 1
+      s1_iter += 1
+      s2_iter += 1
+  return num_common
 
 def ListSimilarity(s1, s2):
   """Returns the size of the largest common subset of s1 and s2
-  elements divided by their length combined."""
+  elements divided by their length combined or 0 if both are empty."""
   if not s1 and not s2:
     return 0
   return NumCommonElements(s1, s2) / (1.0 * (len(s1) + len(s2)))
 
 def HaveCommonElement(s1, s2):
+  """Returns True if s1 and s2 have at least one element in common,
+  False otherwise."""
   return NumCommonElements(s1, s2) > 0
 
 def EnumeratePairs(lst):
-  for i in range(0, len(lst)):
-    for j in range(i + 1, len(lst)):
-      yield (lst[i], lst[j])
+  """Enumerates all pairs in lst, without repeats ((A, B) and (B, A)
+  are considered the same) and (A, A) pairs."""
+  for first in range(0, len(lst)):
+    for second in range(second + 1, len(lst)):
+      yield (lst[first], lst[second])
 
 
 def Flatten(lst):
+  """Converts a list of lists into a list containing all elements
+  from any element of the previous list."""
   lst_sum = []
   for item in lst:
     lst_sum.extend(item)
