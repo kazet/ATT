74ed0d7af2a04abb74a3848d5ffa607d55a97e20
Krzysztof Zajac <kazet@0x04.net>
2013-12-20 22:10:00 +0100

    we can use aligners that don't require training in test_aligner.py

--- test_aligner.py
+++ test_aligner.py
@@ -5,6 +5,7 @@ import sys
 import nltk
 import argparse
 from att.log import LogDebug
+from att.aligner import AlignerFactory
 from att.pickle import LoadFromFile
 from att.corpus import CorpusFactory
 from att.dictionary import DictionaryFactory
@@ -13,8 +14,10 @@ from att.global_context import global_context
 def main():
   parser = argparse.ArgumentParser(description='Align a corpus.')
   parser.add_argument('--trained_aligner',
-                      help="Trained aligner (written by train.py) to load.",
-                      required=True)
+                      help="Trained aligner (written by train.py) to load.")
+  parser.add_argument('--aligner_configuration',
+                      help="Configuration of an aligner that doesn't require"
+                           " training.")
   parser.add_argument('--corpus',
                       help="The corpus our aligner will be tested on.",
                       required=True)
@@ -32,13 +35,23 @@ def main():
   args = parser.parse_args(sys.argv[1:])
   global_context.SetArgs(args)
 
+  if (not args.trained_aligner and not args.aligner_configuration) or \
+     (args.trained_aligner and args.aligner_configuration):
+    raise Exception("Please provide exactly one from --trained_aligner and"
+                    " --aligner_configuration")
+
   current_directory = os.path.dirname(__file__)
   nltk.data.path.append(os.path.join(current_directory, "venv/nltk_data"))
 
   LogDebug("[test_aligner.py] loading corpus...")
   corpus = CorpusFactory.MakeFromFile(args.corpus)
   LogDebug("[test_aligner.py] loading aligner...")
-  aligner = LoadFromFile(args.trained_aligner)
+  if args.trained_aligner:
+    aligner = LoadFromFile(args.trained_aligner)
+  elif args.aligner_configuration:
+    aligner = AlignerFactory.MakeFromFile(args.aligner_configuration)
+  else:
+    assert(False)
   LogDebug("[test_aligner.py] loading dictionary...")
   dictionary = DictionaryFactory.MakeFromFile(args.dictionary)
   LogDebug("[test_aligner.py] testing aligner...")
