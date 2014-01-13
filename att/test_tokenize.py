4764f0a3c401e7ce8b564fd40f1e018a7b105093
Krzysztof Zajac <kazet@0x04.net>
2014-01-13 13:23:14 +0100

    naming fix in att/test_tokenize.py

--- att/test_tokenize.py
+++ att/test_tokenize.py
@@ -2,6 +2,6 @@ from att.test import TestCase
 from att.test import MockWritableFile
 from att.tokenize import word_tokenize
 
-class LogTestCase(TestCase):
+class TokenizeTestCase(TestCase):
   def test_word_tokenize(self):
     self.assertEqual(word_tokenize("Jestem pierogiem."), ["Jestem", "pierogiem"])
