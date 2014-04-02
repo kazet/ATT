6f5c11fc022292d674e3184149c2ba125a8611c2
Krzysztof Zajac <kazet@0x04.net>
2014-04-02 17:17:00 +0200

    better initial bucket values

---
+++ att/aligner/sentence_similarity_signals/exceptions.py
@@ -0,0 +1,2 @@
+class TrainingRequiredException(Exception):
+  pass
--- att/aligner/sentence_similarity_signals/signals_initial_bucket_value_collection.py
+++ att/aligner/sentence_similarity_signals/signals_initial_bucket_value_collection.py
@@ -1,7 +1,11 @@
+import os
+
 class SignalsInitialBucketValueCollection(object):
   """Stores the default signal bucket values (without any training performed)
   created by smoothing from data returned by different training."""
-  DEFAULT_BUCKET_VALUES_REPR = ''
+  DEFAULT_BUCKET_VALUES_REPR = \
+      open(os.path.join(os.path.dirname(__file__),
+                        'signals_initial_bucket_values')).read()
 
   def __init__(self, bucket_values_repr=None):
     if not bucket_values_repr:
@@ -12,7 +16,7 @@ class SignalsInitialBucketValueCollection(object):
     for record in self._bucket_values_repr.split('|'):
       tokens = record.split(' ')
       signal_name = tokens[0]
-      self._bucket_values[signal_name] = tokens[1:]
+      self._bucket_values[signal_name] = [float(token) for token in tokens[1:]]
 
   def GetBucketValuesFor(self, signal_name):
     return self._bucket_values[signal_name]
