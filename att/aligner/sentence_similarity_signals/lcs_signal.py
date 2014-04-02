42eed1b7b69f31f980de08b5906a23cda6dc6a2f
Krzysztof Zajac <kazet@0x04.net>
2014-04-02 17:18:14 +0200

    LCSSignal now accepts strings

--- att/aligner/sentence_similarity_signals/lcs_signal.py
+++ att/aligner/sentence_similarity_signals/lcs_signal.py
@@ -20,8 +20,8 @@ class LCSSignal(Signal):
                     unused_lang2, sentence2,
                     unused_dictionary):
     """Compute the signal value."""
-    assert(type(sentence1).__name__ == 'unicode')
-    assert(type(sentence2).__name__ == 'unicode')
+    assert(type(sentence1).__name__ in ['str', 'unicode'])
+    assert(type(sentence2).__name__ in ['str', 'unicode'])
     prefix1 = unidecode(sentence1[:self._max_length]).lower()
     prefix2 = unidecode(sentence2[:self._max_length]).lower()
     return len(LongestCommonSubstring(prefix1, prefix2)) / float(len(prefix1) + len(prefix2))
